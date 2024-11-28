27542be1f12de3bf336a4b61589e326925c3b327
mz-main-12 <mz-main-12@gmail.com>
2024-11-28 19:19:08 +0800

    feat: mediaview interup

--- app/src/main/java/info/hermiths/chatapp/MainActivity.kt
+++ app/src/main/java/info/hermiths/chatapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -15,6 +16,7 @@ import info.hermiths.chatapp.ui.presentation.components.NavRoute
 
 import info.hermiths.chatapp.ui.presentation.screen.ChatScreen
 import info.hermiths.chatapp.ui.presentation.screen.MediaViewer
+import info.hermiths.chatapp.ui.presentation.viewmodel.ChatScreenViewModel
 import info.hermiths.chatapp.ui.theme.ChatAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +26,7 @@ class MainActivity : ComponentActivity() {
             ChatAppTheme {
                 val navController = rememberNavController()
                 val context = LocalContext.current
+                val viewModel: ChatScreenViewModel = viewModel()
 
                 val gifImageLoader = ImageLoader.Builder(context).components {
                     if (SDK_INT >= 28) {
@@ -37,13 +40,13 @@ class MainActivity : ComponentActivity() {
                     navController = navController, startDestination = NavRoute.CHAT
                 ) {
                     composable(route = NavRoute.CHAT) {
-                        ChatScreen(navController, imageLoader = gifImageLoader)
+                        ChatScreen(navController, viewModel, imageLoader = gifImageLoader)
                     }
                     composable(route = "${NavRoute.MEDIA_VIEWER}/{msgClientId}") { navBackStackEntry ->
                         val msgClientId = navBackStackEntry.arguments?.getString("msgClientId")
                         msgClientId?.let {
                             MediaViewer(
-                                navController, msgClientId, gifImageLoader
+                                navController, viewModel, msgClientId, gifImageLoader
                             )
                         }
                     }
