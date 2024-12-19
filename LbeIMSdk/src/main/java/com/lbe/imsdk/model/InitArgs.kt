32b7f492675b7c1156df84cc2bf3588bbe3fbf78
mz-main-12 <mz-main-12@gmail.com>
2024-12-19 15:41:00 +0800

    1.1.8; InitArgs modify, modify comileSdk

--- LbeIMSdk/src/main/java/com/lbe/imsdk/model/InitArgs.kt
+++ LbeIMSdk/src/main/java/com/lbe/imsdk/model/InitArgs.kt
@@ -1,5 +1,15 @@
 package com.lbe.imsdk.model
 
 data class InitArgs(
-    val lbeSign: String, val nickId: String, val nickName: String, val lbeIdentity: String
+    val lbeSign: String,
+    val lbeIdentity: String,
+    val nickId: String,
+    val nickName: String,
+    val phone: String,
+    val email: String,
+    val headerIcon: String,
+    val language: String,
+    val device: String,
+    val source: String,
+//    val extraInfo: MutableMap<String, *>?,
 )
